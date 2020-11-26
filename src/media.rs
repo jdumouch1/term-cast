9818fb63ff8bdba08d60eed7484ae8b2df5e3414
Jayden Dumouchel <jdumouch@ualberta.ca>
2020-11-26 10:51:52 -0700

    Rename fileserver to media Added basic ffmpeg conversion stub

--- src/media.rs
+++ src/media.rs
@@ -3,6 +3,8 @@
 use tokio::sync::oneshot;
 use std::net::UdpSocket;
 
+use std::process::Command;
+
 pub async fn host_media(media: String) -> Result<(String, oneshot::Sender<()>), std::io::Error> {
     // Get local ip address
     let local_ip = get_local_ip()?;
@@ -17,6 +19,36 @@ pub async fn host_media(media: String) -> Result<(String, oneshot::Sender<()>), 
     tokio::task::spawn(server);
     Ok((media_addr, shutdown_sender))
 }
+
+pub fn convert_to_mp4(input: String) { 
+    // let probe = Command::new("sh")
+    //     .arg("-c")
+    //     .arg(format!("ffprobe -v error -select_streams v:0 -show_entries stream=codec_name \
+    //     -of default=noprint_wrappers=1:nokey=1 {}", input))
+    //     .output()
+    //     .unwrap();
+
+
+    // TODO: Windows compatability
+    let mut ffmpeg = Command::new("sh")
+        .arg("-c")
+        .arg(format!("ffmpeg -y -i {} -c copy host_media.mp4", input))
+        .spawn()
+        .unwrap();
+
+    loop {
+        match &ffmpeg.try_wait() {
+            Ok(_) => {
+                if let Some(out) = ffmpeg.stdout{
+                    println!("{:?}", out);                    
+                }
+                break;
+            },
+            Err(_) => {},
+        };
+    };
+}
+
 
 fn get_local_ip() -> Result<String, std::io::Error> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
